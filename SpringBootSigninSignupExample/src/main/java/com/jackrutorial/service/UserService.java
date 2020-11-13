9ad9925dc5c51a18763640130ff68d00b8fcb8cc
younes <BOUKHTACHE@DESKTOP-4LTB3DH>
2020-11-13 14:57:36 +0100

    this is my project login and registration

---
+++ SpringBootSigninSignupExample/src/main/java/com/jackrutorial/service/UserService.java
@@ -0,0 +1,10 @@
+package com.jackrutorial.service;
+
+import com.jackrutorial.model.User;
+
+public interface UserService {
+  
+ public User findUserByEmail(String email);
+ 
+ public void saveUser(User user);
+}
